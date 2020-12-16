9076bf7a11b5ced2ec9697cf00ac682ac6e39cc6
tyronetheartist <74928137+tyronetheartist@users.noreply.github.com>
2020-12-16 18:22:19 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -87,7 +87,7 @@ function draw() {
 
   var mytable = document.getElementById('myTable');
   var images = mytable.getElementsByTagName('img');
-  console.log("draw has been called");
+
 
 
   // Loop through all images
@@ -118,5 +118,5 @@ function draw() {
       ctx.drawImage(document.getElementById('frame'), 0, 0, 150, 150);
     }
   }
-  console.log("draw has finished");
+  
 }
