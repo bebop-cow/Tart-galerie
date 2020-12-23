1071f57b38aba8ff79a50375f69c33ef96f2b6f0
tyronetheartist <74928137+tyronetheartist@users.noreply.github.com>
2020-12-23 16:11:15 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -127,17 +127,37 @@ $(document).ready(function() {
     };
   });
 });
+function saveAs(uri, filename) {
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+          link.href = uri;
+          link.download = filename;
+
+          //Firefox requires the link to be in the body
+          document.body.appendChild(link);
+
+          //simulate click
+          link.click();
+
+          //remove the link when done
+          document.body.removeChild(link);
+        } else {
+          window.open(uri);
+        }
+      }
 
 
 $(document).ready(function() {
   $("#foo").click(function() {
     var convertMeToImg = $('#myTable')[0];
     html2canvas(convertMeToImg, {
+      scale: 4,
       allowTaint: true,
       useCORS: true,
-      logging: false,
+      logging: true,
     }).then(function(canvas) {
       $('#preview').append(canvas);
+      saveAs(canvas.toDataURL(), 'canvas.png');
     });
   });
 
